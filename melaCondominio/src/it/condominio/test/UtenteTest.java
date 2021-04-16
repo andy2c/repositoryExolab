1544725255c964c489e7aaac002a1594984a7e3d
paoli <paoli@192.168.1.108>
2021-04-16 11:48:26 +0200

    testato

--- melaCondominio/src/it/condominio/test/UtenteTest.java
+++ melaCondominio/src/it/condominio/test/UtenteTest.java
@@ -13,28 +13,7 @@ public class UtenteTest {
 		Utente model = new Utente();
 		model.setId(4);
 		model.setPassword("passCambiata");
-	//	Utente utente = crud.find(3);
-//		utente = crud.find(4);
-	//	crud.insert(model);
-		
-	//	crud.update(utente);
-	//	crud.delete(5);
-		
-	//	System.out.println(utente.toString());
-		
-//		List<Utente>  lista = crud.findAll();
-//		
-//		
-//		for(Utente u : lista)
-//			System.out.println(u.toString());
-		
-//		Utente test = crud.findByEmailAndPassword(model);
-//		
-//		System.out.println(test.toString());
-		
-//		crud.updatePassword(model);
-		
-		
+
 		System.out.println("Stampa qui");
 		
 		
