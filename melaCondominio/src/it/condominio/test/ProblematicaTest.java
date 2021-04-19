668894e833226b710098f7d53ca326f8f7e4e771
paoli <paoli@192.168.200.138>
2021-04-19 17:11:01 +0200

    Problematica fatta

---
+++ melaCondominio/src/it/condominio/test/ProblematicaTest.java
@@ -0,0 +1,45 @@
+package it.condominio.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.condominio.crud.ProblematicaCRUD;
+import it.condominio.model.Problematica;
+
+public class ProblematicaTest {
+
+	public static void main(String[] args) {
+		Problematica test = new Problematica();
+		ProblematicaCRUD crud = new ProblematicaCRUD();
+		List<Problematica> list = new ArrayList<Problematica>();
+		Problematica model = new Problematica(2,"cacchioooo", "prova", "Pooooo", 1, 1, null, null);
+		
+		
+//	   test = crud.find(1);
+//		
+//		
+//		
+//		System.out.println(test.toString());
+//		
+//		
+//		list = crud.findAll();
+//		
+//		for(Problematica p : list)
+//            System.out.println(p.toString());		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
