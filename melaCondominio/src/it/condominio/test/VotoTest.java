ed5c7ce58d100c1f2d90dc38cae1ad5f3373e5be
paoli <paoli@192.168.200.138>
2021-04-21 11:52:03 +0200

    Voto fatto al 90 %

---
+++ melaCondominio/src/it/condominio/test/VotoTest.java
@@ -0,0 +1,35 @@
+package it.condominio.test;
+
+import it.condominio.crud.VotoCRUD;
+import it.condominio.model.Voto;
+
+public class VotoTest {
+
+	public static void main(String[] args) {
+	    Voto test = new Voto();
+	    VotoCRUD crud = new VotoCRUD();
+	    Voto model = new Voto(1, 1, 1);
+	    
+//		crud.insert(model);
+		
+//		test = crud.find(1);
+//		System.out.println(test.toString());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
