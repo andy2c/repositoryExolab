76bdab8c4718a3af1a3f882eca586c3cea0f66d4
paoli <paoli@192.168.200.138>
2021-04-21 10:33:58 +0200

    Filtro_riunione fatto , come accordato solo l'insert

---
+++ melaCondominio/src/it/condominio/test/Filtro_riunioneTest.java
@@ -0,0 +1,32 @@
+package it.condominio.test;
+
+import it.condominio.crud.Filtro_riunioneCRUD;
+import it.condominio.model.Filtro_riunione;
+
+public class Filtro_riunioneTest {
+
+	public static void main(String[] args) {
+	    Filtro_riunione model =  new Filtro_riunione(2, 1);
+		
+	    Filtro_riunioneCRUD  crud = new Filtro_riunioneCRUD();
+		
+		
+		
+		
+//		crud.insert(model);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
