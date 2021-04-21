77c2948e597e3e8d8695931f0e4da2ea60cf6beb
paoli <paoli@192.168.200.138>
2021-04-21 10:21:58 +0200

    Filtro_delega fatto ( solo l'insert come stabilito)

---
+++ melaCondominio/src/it/condominio/test/Filtro_delegaTest.java
@@ -0,0 +1,35 @@
+package it.condominio.test;
+
+import it.condominio.crud.Filtro_delegaCRUD;
+import it.condominio.model.Filtro_delega;
+
+public class Filtro_delegaTest {
+
+	public static void main(String[] args) {
+		Filtro_delegaCRUD crud = new Filtro_delegaCRUD();
+		Filtro_delega model = new Filtro_delega(7, 1, 1);
+		
+		
+		
+		//crud.insert(model);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
